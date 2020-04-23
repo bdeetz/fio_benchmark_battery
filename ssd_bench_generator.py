351f94e69e6700e8e3d6eb1c87104aa7e775f51d
Brady Deetz <bdeetz@gmail.com>
2020-04-23 11:54:35 -0500

    add wait code

--- ssd_bench_generator.py
+++ ssd_bench_generator.py
@@ -37,9 +37,23 @@ for job_count in job_counts:
     for read_percent in read_percents:
         for block_size in block_sizes:
             for io_depth in io_depths:
+                print('pids=()')
                 for device in devices:
                     print('cd %s/%d/%d/%s/%d/direct' % (device, job_count, read_percent, block_size, io_depth))
                     print('%s --filename=/mnt/%s/fio-5g --name=%d_%d_%s_%d_direct --direct=1 --sync=1 --numjobs=%d --iodepth=%d --bs=%s --rw=randrw --rwmixread=%d --write_bw_log=%d_%d_%s_%d_direct_bw.results --write_iops_log=%d_%d_%s_%d_direct_iops.results --write_lat_log=%d_%d_%s_%d_direct_lat.results&' % (base_command, device, job_count, read_percent, block_size, io_depth, job_count, io_depth, block_size, read_percent, job_count, read_percent, block_size, io_depth, job_count, read_percent, block_size, io_depth, job_count, read_percent, block_size, io_depth))
+                    print('pids+=($?)')
                     print('cd $basedir')
+                    
+                print('pids=()')
+
+                print('for pid in ${pids[@]}')
+                print('do')
+                print('    ret=`kill -0 ${pid}`')	
+                print('    while [[ ${ret} -ne 0 ]]')
+                print('    do')
+                print('        sleep 1')
+                print('        ret=`kill -0 ${pid}`')
+                print('    done')
+                print('done')
 
 print('cd ..')
