0334b508aa527af3fded3ab951fdff428f59d75e
Brady Deetz <bdeetz@gmail.com>
2020-04-23 11:44:29 -0500

    update run for deb

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-yum install -y fio
+#yum install -y fio
+apt-get install -y fio
 
 python ssd_bench_generator.py > ssd_bench.sh
 sh -x ssd_bench.sh 2>&1 | tee `pwd`/benchmark.log
