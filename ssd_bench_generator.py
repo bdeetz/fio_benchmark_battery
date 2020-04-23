19b8be911d1bd4d978169d1e139798eebc65c109
Brady Deetz <bdeetz@gmail.com>
2020-04-23 15:19:13 -0500

    burn burst credits

--- ssd_bench_generator.py
+++ ssd_bench_generator.py
@@ -19,8 +19,52 @@ for i in range(0, num_devices):
         
     devices.append('%s%s' % (device_prefix, device_suffix))
 
-base_command = 'fio --ioengine=libaio --invalidate=1 --ramp_time=5 --size=%s --runtime=600 --time_based' % (file_size)
+# we're lazy and repeating this code... don't judge
+# first run of this code is to burn all of the burst credits which takes 30 minutes
+base_command = 'fio --ioengine=libaio --invalidate=1 --ramp_time=5 --size=%s --runtime=1860 --time_based' % (file_size)
 print('#!/usr/bin/env bash')
+print('mkdir results')
+print('cd results')
+print('basedir=`pwd`')
+
+for job_count in job_counts:
+    for read_percent in read_percents:
+        for block_size in block_sizes:
+            for io_depth in io_depths:
+                for device in devices:
+                    print('mkdir -p %s/%d/%d/%s/%d/direct' % (device, job_count, read_percent, block_size, io_depth))
+
+
+for job_count in job_counts:
+    for read_percent in read_percents:
+        for block_size in block_sizes:
+            for io_depth in io_depths:
+                print('pids=()')
+                for device in devices:
+                    print('cd %s/%d/%d/%s/%d/direct' % (device, job_count, read_percent, block_size, io_depth))
+                    print('%s --filename=/mnt/%s/fio-5g --name=%d_%d_%s_%d_direct --direct=1 --sync=1 --numjobs=%d --iodepth=%d --bs=%s --rw=randrw --rwmixread=%d --write_bw_log=%d_%d_%s_%d_direct_bw.results --write_iops_log=%d_%d_%s_%d_direct_iops.results --write_lat_log=%d_%d_%s_%d_direct_lat.results&' % (base_command, device, job_count, read_percent, block_size, io_depth, job_count, io_depth, block_size, read_percent, job_count, read_percent, block_size, io_depth, job_count, read_percent, block_size, io_depth, job_count, read_percent, block_size, io_depth))
+                    print('pids+=($?)')
+                    print('cd $basedir')
+                    
+                print('pids=()')
+
+                print('for pid in ${pids[@]}')
+                print('do')
+                print('    ret=`kill -0 ${pid}`')	
+                print('    while [[ ${ret} -ne 0 ]]')
+                print('    do')
+                print('        sleep 1')
+                print('        ret=`kill -0 ${pid}`')
+                print('    done')
+                print('done')
+
+print('cd ..')
+
+
+# second run is the actual benchmark
+base_command = 'fio --ioengine=libaio --invalidate=1 --ramp_time=5 --size=%s --runtime=300 --time_based' % (file_size)
+print('#!/usr/bin/env bash')
+print('rm -rf results')
 print('mkdir results')
 print('cd results')
 print('basedir=`pwd`')
